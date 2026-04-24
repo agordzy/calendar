e6f3e318d16d5e2ba92325f5873acb70d5bd17fc
agordzy <agordy2006@gmail.com>
2026-04-24 20:01:40 +0300

    добавлено состояние и addTask

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (newTask) => {
+        setTasks([...tasks, newTask]);
+    };
+
     return (
         <div className="App">
-            <h1>📅 Календарь дедлайнов</h1>
+            <h1>Календарь дедлайнов</h1>
+            <TaskForm onAddTask={addTask} />
+            <TaskList tasks={tasks} />
         </div>
     );
 }
